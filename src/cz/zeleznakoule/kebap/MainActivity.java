b612db1293e0194fcd11ea6f592e1fc8554a168d
Tkadla <tkadlecek.j@gmail.com>
2012-08-07 23:55:33 +0200

    Zakladni aktivita, zalozeni dedicnosti aktivit, nastaveni zakladnich barev a stylu a jejich dedicnosti

--- src/cz/zeleznakoule/kebap/MainActivity.java
+++ src/cz/zeleznakoule/kebap/MainActivity.java
@@ -3,19 +3,43 @@ package cz.zeleznakoule.kebap;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
-public class MainActivity extends Activity {
+public class MainActivity extends BaseActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        createTabs();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    /**
+     * Vytvoreni a naplneni tabu a tabhostu v layout
+     */
+    private void createTabs(){
+    	TabHost tabs = (TabHost)findViewById(R.id.tabhost);
+    		tabs.setup();
+    	TabSpec specs = tabs.newTabSpec("feed");
+    		specs.setContent(R.id.feedTab);
+    		specs.setIndicator("Feed");
+    		tabs.addTab(specs);
+    		
+    		specs.setContent(R.id.calendarTab);
+    		specs.setIndicator("Calendar");
+    		tabs.addTab(specs);
+    		
+    		specs.setContent(R.id.statsTab);
+    		specs.setIndicator("Stats");
+    		tabs.addTab(specs);
     }
 
     
