51f42bc4f8deeb69f29c4eebfa06918865d98694
iskuda <iskuda@gmail.com>
2012-08-08 10:53:29 +0200

    Upravení importů

--- src/cz/zeleznakoule/kebap/model/SqlHelper.java
+++ src/cz/zeleznakoule/kebap/model/SqlHelper.java
@@ -2,7 +2,6 @@ package cz.zeleznakoule.kebap.model;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class SqlHelper extends SQLiteOpenHelper {
